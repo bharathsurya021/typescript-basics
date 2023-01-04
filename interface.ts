6a8f527198003bb683759c345096a29e9a17470c
bharathsurya021 <bharathsurya021@gmail.com>
2023-01-04 15:55:30 +0530

    class and inheritance

--- interface.ts
+++ interface.ts
@@ -34,3 +34,36 @@ let user = {
 };
 
 details(user);
+
+//Class
+
+class Employee {
+    employeeName : string;
+
+    constructor(name: string){
+        this.employeeName = name;
+    }
+
+    greet (){
+        console.log(`Good Morning ${this.employeeName}`);
+    }
+}
+
+let employeeNew =  new Employee ('Bharath');
+console.log(employeeNew.employeeName);
+employeeNew.greet()
+
+class Manager extends Employee {
+    constructor (managername: string){
+        super(managername);
+    }
+
+    delegateWork(){
+        console.log(`Manager delegating tasks`);
+    }
+}
+
+let m1 = new Manager('Karthik')
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
